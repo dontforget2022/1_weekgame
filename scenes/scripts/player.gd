4099e570c73fcd386125f9bfc9c540bdce689ed0
sodasora <nikolassava07@gmail.com>
2024-08-12 12:38:08 +0200

    Added lasers

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 @export var speed = 200
+var can_shoot: bool = true
+
+signal laser(pos)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,5 +14,16 @@ func _process(delta):
 	var	direction = Input.get_vector("left", "right","up","down");#getting directions
 
 	velocity = direction*speed;
+	if Input.is_action_just_pressed("shoot") and can_shoot: 
+		laser.emit($laser_start_pos.global_position)
+		can_shoot = false;
+		$can_shoot.start()
 	move_and_slide() 
 	
+
+
+
+
+
+func _on_can_shoot_timeout():
+	can_shoot = true
